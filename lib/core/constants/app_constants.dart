ee05b5dad1c7964853ea5d9e4eff4035706a794b
E.Michael <elias.g.mikhael@gmail.com>
2023-05-21 13:25:49 +0300

    added app constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppConstants {
+  static const String APP_TITLE = 'Ososs Test';
+  static EdgeInsets screenPadding = EdgeInsets.symmetric(horizontal: 20.w);
+
+  /// PNG
+  static const String PLACEHOLDER_IMG = 'assets/images/png/placeholder.png';
+
+  /// SVG
+  static const String FLUTTER_LOGO_SVG = 'assets/images/svg/flutter.svg';
+}
